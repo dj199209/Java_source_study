3006f274eda032b7a6e40730aef6b35f9b9c71b3
戴骥 <329990887@qq.com>
2020-04-28 17:30:12 +0800

    Java源码阅读环境搭建

---
+++ src/com/dj/study/Dome.java
@@ -0,0 +1,17 @@
+package com.dj.study;
+
+import java.util.HashMap;
+
+/**
+ * @author D_Jaye
+ */
+public class Dome {
+    public static void main(String[] args) {
+
+        HashMap hashMap = new HashMap();
+        hashMap.put("1",1);
+        hashMap.put("1",1);
+        hashMap.put("1",1);
+        hashMap.put("1",1);
+    }
+}
